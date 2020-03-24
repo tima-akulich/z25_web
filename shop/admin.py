4bd425abdbeb7792102e161644fdfdfd8fe00fdf
arseny <semchenko99@mail.ru>
2020-03-24 17:09:05 +0300

    admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,32 @@
-from django.contrib import admin  # noqa
+from django.contrib import admin
+from .models import Product
+from .models import Basket
+from .models import BasketElements
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('number', 'title', 'price', 'amount', '__str__', 'id')
+    search_fields = ('title', 'number')
+    list_display_links = ('__str__', 'id')
+    list_editable = ('number', 'title')
+    list_filter = ('title', 'price', 'amount')
+    sortable_by = ('price', 'amount')
+
+
+class BasketAdmin(admin.ModelAdmin):
+    list_display = ('number', 'user', '__str__', 'status', 'id')
+    search_fields = ('user', 'number')
+    list_display_links = ('__str__', 'id')
+
+
+class BasketElementsAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'id')
+    search_fields = ('user', 'element')
+    list_display_links = ('__str__', 'id')
+
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Basket, BasketAdmin)
+admin.site.register(BasketElements, BasketElementsAdmin)
+
