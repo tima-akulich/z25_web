461747db55d9cd08fddd312b8ccfddeb1c8a3072
Dmitry Anatsko <anatskodr@gmail.com>
2020-03-27 14:01:54 +0300

    Add task

--- shop/urls.py
+++ shop/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from shop.views import products_list_view
 from shop.views import product_details_view
 from shop.views import try_forms
+from shop.views import categories_view
 
 from django.conf.urls.static import static
 
@@ -11,6 +12,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('forms', try_forms, name='try_forms'),
     path('', products_list_view, name='products'),
+    path('categories', categories_view, name='categories'),
+    path('categories/<slug:category>', categories_view, name='categories_by_category'),
     path('product/<str:pk>', product_details_view, name='product_details'),
     path('<slug:category>', products_list_view, name='products_by_category'),
 ]
@@ -18,4 +21,3 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,10 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from shop.forms import CardForm
 
 
-from shop.models import Product
+from shop.models import Product, Category
 
 
 def hello_world(request):
@@ -21,8 +22,11 @@ def products_list_view(request, category=None):
     products = Product.objects.filter(published=True)
     if category:
         products = products.filter(categories__slug=category)
+    paginator = Paginator(products, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     return render(request, 'products_list.html', context={
-        'products': products,
+        'products': products, 'page_obj': page_obj
     })
 
 
@@ -30,6 +34,19 @@ def product_details_view(request, pk):
     product = get_object_or_404(Product, pk=pk, published=True)
     return render(request, 'product_details.html', context={
         'product': product,
+    })
+
+
+def categories_view(request, category=None):
+    products = Product.objects.filter(published=True)
+    categories = Category.objects.all()
+    if category:
+        products = products.filter(categories__slug=category)
+    paginator = Paginator(products, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'categories.html', context={
+        'products': products, 'page_obj': page_obj, 'categories': categories
     })
 
 
